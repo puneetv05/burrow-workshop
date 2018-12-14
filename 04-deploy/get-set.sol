ca838d8518d6facf744a4cc494b95f0b12537db5
Casey Kuhlman <casey@monax.io>
2018-12-14 12:39:45 +0100

    add get-set contract
    Signed-off-by: Casey Kuhlman <casey@monax.io>

---
+++ 04-deploy/get-set.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.0.0;
+
+contract Storage {
+  int storedData;
+
+  function set(int x) public {
+    storedData = x;
+  }
+
+  function get() constant public returns (int retVal) {
+    return storedData;
+  }
+}
